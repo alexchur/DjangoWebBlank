a77817470add36111b0caf34a7e91b4474c1b79c
Чураков Алексей Олегович <alex@fgu29.kadastr.ru>
2019-02-07 15:53:46 +0300

    Добавьте файлы проекта.

---
+++ DjangoWebBlank/DjangoWebBlank/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for DjangoWebBlank.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', DjangoWebBlank.views.home, name='home'),
+    # url(r'^DjangoWebBlank/', include('DjangoWebBlank.DjangoWebBlank.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+     url(r'^admin/', admin.site.urls),
+]
